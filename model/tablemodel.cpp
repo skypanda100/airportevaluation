f07b3f80228a95ab6dad412b81acae7aa63fd04d
zhengdongtian <236520700@qq.com>
2016-05-04 09:52:27 +0800

    1.tablemodel修改：对应区间用对应的颜色标示

--- model/tablemodel.cpp
+++ model/tablemodel.cpp
@@ -10,17 +10,35 @@ TableModel::~TableModel(){
 
 }
 
+bool TableModel::setHeaderData(int section, Qt::Orientation orientation, const QVariant &value, int role){
+    bool ret = QStandardItemModel::setHeaderData(section, orientation, value, role);
+    m_headerData.append(value);
+    return ret;
+}
+
 bool TableModel::setData(const QModelIndex &index, const QVariant &value, int role){
     bool ret = QStandardItemModel::setData(index, value, role);
-    if(value.toString().compare("3") == 0){
+
+    if(m_headerData[index.column()].toString().compare("完全可飞") == 0){
         this->item(index.row(), index.column())->setBackground(QColor(50, 150, 0));
         this->item(index.row(), index.column())->setTextAlignment(Qt::AlignCenter);
-    }else if(value.toString().compare("2") == 0){
+    }else if(m_headerData[index.column()].toString().compare("限制可飞") == 0){
         this->item(index.row(), index.column())->setBackground(QColor(255, 180, 0));
         this->item(index.row(), index.column())->setTextAlignment(Qt::AlignCenter);
-    }else if(value.toString().compare("1") == 0){
+    }else if(m_headerData[index.column()].toString().compare("不可飞") == 0){
         this->item(index.row(), index.column())->setBackground(QColor(220, 50, 0));
         this->item(index.row(), index.column())->setTextAlignment(Qt::AlignCenter);
+    }else{
+        if(value.toString().compare("3") == 0){
+            this->item(index.row(), index.column())->setBackground(QColor(50, 150, 0));
+            this->item(index.row(), index.column())->setTextAlignment(Qt::AlignCenter);
+        }else if(value.toString().compare("2") == 0){
+            this->item(index.row(), index.column())->setBackground(QColor(255, 180, 0));
+            this->item(index.row(), index.column())->setTextAlignment(Qt::AlignCenter);
+        }else if(value.toString().compare("1") == 0){
+            this->item(index.row(), index.column())->setBackground(QColor(220, 50, 0));
+            this->item(index.row(), index.column())->setTextAlignment(Qt::AlignCenter);
+        }
     }
 
     return ret;
--- model/tablemodel.h
+++ model/tablemodel.h
@@ -9,7 +9,11 @@ public:
     TableModel(int, int, QObject *parent = 0);
     ~TableModel();
 
+    bool setHeaderData(int section, Qt::Orientation orientation, const QVariant &value, int role = Qt::EditRole);
     bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole);
+
+private:
+    QList<QVariant> m_headerData;
 };
 
 #endif // TABLEMODEL
